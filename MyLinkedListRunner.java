dc52cd028b4c0418d90349525e3d6765c75da091
snehak2299 <skanase2299@gmail.com>
2021-08-24 19:08:18 +0530

    [sneha]:basic set up is done

---
+++ MyLinkedListRunner.java
@@ -0,0 +1,10 @@
+package com.bridglab.linkedlist;
+
+public class MyLinkedListRunner {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ MyNode.java
@@ -0,0 +1,31 @@
+package com.bridglab.linkedlist;
+
+public class MyNode<K>{
+	private K key;
+	private MyNode next;
+	
+	public MyNode(K key) {
+		super();
+		this.key = key;
+		this.next = null;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public MyNode getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
+	
+	
+
+}
