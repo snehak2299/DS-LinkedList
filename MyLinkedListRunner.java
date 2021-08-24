f959c5f935f73ad8a71ae8a3c1d3e9e9ac7d0735
snehak2299 <skanase2299@gmail.com>
2021-08-24 19:11:06 +0530

    [sneha]:created simple linked list of 56, 30, 70

--- MyLinkedListRunner.java
+++ MyLinkedListRunner.java
@@ -3,7 +3,13 @@ package com.bridglab.linkedlist;
 public class MyLinkedListRunner {
 
 	public static void main(String[] args) {
-		
+		MyNode<Integer> firstNode=new MyNode<Integer>(56);
+        MyNode<Integer> secondNode=new MyNode<Integer>(30);
+        MyNode<Integer> thirdNode=new MyNode<Integer>(70);
+        firstNode.setNext(secondNode);
+        secondNode.setNext(thirdNode);
+        boolean result = firstNode.getNext().equals(secondNode)&& secondNode.getNext().equals(thirdNode);
+        System.out.println(result);
 
 	}
 
